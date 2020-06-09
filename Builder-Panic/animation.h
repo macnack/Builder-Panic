8633d6121c57576315685c01d90bbc2595be0409
macko-manjaro <krupka.maciej@icloud.com>
2020-06-10 01:00:55 +0200

    animation class

--- Builder-Panic/animation.h
+++ Builder-Panic/animation.h
@@ -2,5 +2,23 @@
 #define ANIMATION_H
 #include "gameobject.h"
 
+class Animation : public sf::Sprite
+{
+public:
+    Animation(const sf::Vector2f &pos, const sf::Texture &texture,
+         const sf::IntRect &frame,
+         const float &framerate, const int &length);
+    void addAnimation(const int length);
+    void playAnimation(const float &dt);
+
+private:
+    float framerate_ = 0.f;
+    sf::Texture texture_;
+    sf::IntRect frame_;
+    sf::IntRect startRect;
+    sf::IntRect currentRect;
+    sf::IntRect endRect;
+    float timer = 0.f;
+};
 
 #endif // ANIMATION_H
--- Builder-Panic/coin.h
+++ Builder-Panic/coin.h
@@ -1,23 +1,19 @@
 #ifndef COIN_H
 #define COIN_H
-#include "gameobject.h"
-#include "entity.h"
-class Coin : public GameObject
+#include "animation.h"
+
+class Coin : public Animation
 {
 public:
     Coin(const sf::Vector2f &pos, const sf::Texture &texture,
          const sf::IntRect &frame,
-         const float &framerate, const int &length);
-    void addAnimation(const int length);
-    void playAnimation(const float &dt);
-
+         const float &framerate, const int &length): Animation(pos, texture,frame,framerate, length){
+         value = 100;
+         bound = this->getLocalBounds();
+    }
 private:
-    float framerate_ = 0.f;
-    sf::IntRect frame_;
-    sf::IntRect startRect;
-    sf::IntRect currentRect;
-    sf::IntRect endRect;
-    float timer = 0.f;
+    int value;
+    sf::FloatRect bound;
 };
 
 #endif // COIN_H
