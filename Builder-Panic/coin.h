b8455cb353c01addbd95d4d59bfc61fe5d29b3ae
macko-manjaro <krupka.maciej@icloud.com>
2020-06-09 21:55:23 +0200

    id

--- Builder-Panic/coin.h
+++ Builder-Panic/coin.h
@@ -5,7 +5,9 @@
 class Coin : public GameObject
 {
 public:
-    Coin();
+    Coin(const sf::Vector2f &pos, const sf::Texture &texture) : GameObject(pos, sf::FloatRect(0,0,0,0),texture){
+
+    }
 };
 
 #endif // COIN_H
