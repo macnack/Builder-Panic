d50486b9edafe1af9e5c80c46a92ebba44f6f0bc
macko-manjaro <krupka.maciej@icloud.com>
2020-06-11 14:05:16 +0200

    update collision

--- Builder-Panic/entity.cpp
+++ Builder-Panic/entity.cpp
@@ -18,9 +18,9 @@ int Entity::setBounds(const std::vector<std::unique_ptr<sf::Sprite>> &platforms)
 
 void Entity::jump()
 {
-    if (this->grounded == true)
+    if (grounded == true)
     {
-        this->velocity.y -= 1000;
+        velocity.y -= 1000;
     }
     if (current_stage > 1)
         current_stage -= 1;
@@ -29,9 +29,9 @@ void Entity::jump()
 
 void Entity::down()
 {
-    if (this->stage_down == true && current_stage < 4)
+    if (stage_down == true && current_stage < 4)
     {
-        this->velocity.y += 1000;
+        velocity.y += 1000;
         current_stage += 1;
         next_stage = current_stage;
     }
@@ -40,47 +40,48 @@ void Entity::down()
 void Entity::moveSprite(const sf::Vector2f &dir, const float &dt)
 {
     //incrasing speed in the inputed dirction
-    this->velocity += dir * (dt * this->acceleration);
+    velocity += dir * (dt * acceleration);
 }
 
 void Entity::updateGravity(const float &dt)
 {
     //applying force of gravity
-    this->velocity.y += this->gravity * dt;
+    velocity.y += gravity * dt;
 }
 
 void Entity::updateMovement(const float &dt)
 {
     this->updateGravity(dt);
-    if (this->velocity.y > 0.f)
+    if (velocity.y > 0.f)
     {
         //Max falling velocity check
-        if (this->velocity.y > this->maxFallingVelocity)
+        velocity.y -= 1/5.f * gravity * dt;
+        if (velocity.y > maxFallingVelocity)
         {
-            this->velocity.y = this->maxFallingVelocity;
+            velocity.y = maxFallingVelocity;
         }
     }
-    if (this->velocity.x > 0.f)
+    if (velocity.x > 0.f)
     { //right
         //deceleration
-        this->velocity.x -= this->deceleration * dt;
-        if (this->velocity.x < 0.f)
-            this->velocity.x = 0.f;
+        velocity.x -= deceleration * dt;
+        if (velocity.x < 0.f)
+            velocity.x = 0.f;
         //max velocity check
-        if (this->velocity.x > this->maxVelocity)
-            this->velocity.x = maxVelocity;
+        if (velocity.x > maxVelocity)
+            velocity.x = maxVelocity;
     }
-    else if (this->velocity.x < 0.f)
+    else if (velocity.x < 0.f)
     { //if going left
         //decelaretion
-        this->velocity.x += deceleration * dt;
-        if (this->velocity.x > 0.f)
-            this->velocity.x = 0.f;
+        velocity.x += deceleration * dt;
+        if (velocity.x > 0.f)
+            velocity.x = 0.f;
         //max velocity check
-        if (this->velocity.x < -this->maxVelocity)
-            this->velocity.x = -this->maxVelocity;
+        if (velocity.x < -maxVelocity)
+            velocity.x = -maxVelocity;
     }
-    this->move(this->velocity * dt);
+    this->move(velocity * dt);
 }
 
 void Entity::updateCollisions(const std::vector<std::unique_ptr<sf::Sprite>> &platforms, const float &dt)
@@ -88,31 +89,31 @@ void Entity::updateCollisions(const std::vector<std::unique_ptr<sf::Sprite>> &pl
     grounded = false;
     sf::FloatRect playerBounds = this->getGlobalBounds();
     sf::FloatRect playerBoundsNext = this->getGlobalBounds();
-    playerBoundsNext.left = this->getPosition().x + this->velocity.x * dt;
-    playerBoundsNext.top = this->getPosition().y + this->velocity.y * dt;
+    playerBoundsNext.left = getPosition().x + velocity.x * dt;
+    playerBoundsNext.top = getPosition().y + velocity.y * dt;
     current_stage = setBounds(platforms);
-    sf::FloatRect platformBounds = platforms[next_stage]->getGlobalBounds();
-    if (platformBounds.intersects(playerBoundsNext))
+    sf::FloatRect next_stagePlatformBounds = platforms[next_stage]->getGlobalBounds();
+    if (next_stagePlatformBounds.intersects(playerBoundsNext))
     { //bottom
-        if (playerBounds.top < platformBounds.top && playerBounds.top + playerBounds.height < platformBounds.top + platformBounds.height && playerBounds.left < platformBounds.left + platformBounds.width && playerBounds.left + playerBounds.width > platformBounds.left)
+        if (playerBounds.top < next_stagePlatformBounds.top && playerBounds.top + playerBounds.height < next_stagePlatformBounds.top + next_stagePlatformBounds.height && playerBounds.left < next_stagePlatformBounds.left + next_stagePlatformBounds.width && playerBounds.left + playerBounds.width > next_stagePlatformBounds.left)
         {
-            this->velocity.y = 0;
-            this->setPosition(playerBounds.left, platformBounds.top - playerBounds.height);
+            velocity.y = -std::abs(velocity.y);
+            this->setPosition(playerBounds.left, next_stagePlatformBounds.top - playerBounds.height);
             grounded = true;
             stage_down = true;
         }
     }
     if (playerBounds.top + playerBounds.height > 800)
     { //zapobiega spadaniu
-        this->setPosition(playerBounds.left, platformBounds.top - playerBounds.height);
+        this->setPosition(playerBounds.left, next_stagePlatformBounds.top - playerBounds.height);
     }
     if ((playerBounds.left + playerBounds.width) < 5)
     { //zapobiega spadaniu
-        this->setPosition(800 - playerBounds.left - playerBounds.width, platformBounds.top - playerBounds.height);
+        this->setPosition(800 - playerBounds.left - playerBounds.width, next_stagePlatformBounds.top - playerBounds.height);
     }
     if ((playerBounds.left) > 795)
     { //zapobiega spadaniu
-        this->setPosition(-(50) / 2.0, platformBounds.top - playerBounds.height);
+        this->setPosition(-(50) / 2.0, next_stagePlatformBounds.top - playerBounds.height);
     }
     this->updateMovement(dt);
 }
